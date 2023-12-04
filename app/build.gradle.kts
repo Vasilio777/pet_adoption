c047849b09b0790702774e3b609c411a292d95eb
v.kotunov <purgen200@gmail.com>
2023-12-04 13:33:01 +0700

    Fix unit tests according to a class presentation. Check couple of test examples

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,15 +73,9 @@ dependencies {
     implementation("androidx.test.ext:junit-ktx:1.1.5")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("androidx.arch.core:core-testing:2.2.0")
-
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.ext:truth:1.5.0")
-    debugImplementation ("androidx.test:runner:1.5.2")
-    debugImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    debugImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
+    testImplementation("org.mockito:mockito-core:5.4.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.0.0")
 }
--- app/src/test/java/com/example/pet_adoption/ExampleUnitTest.kt
+++ app/src/test/java/com/example/pet_adoption/ExampleUnitTest.kt
@@ -1,8 +1,23 @@
 package com.example.pet_adoption
 
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.util.PatternsCompat
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
 
+fun String .isValidEmail(): Boolean {
+
+    return !isNullOrEmpty() && PatternsCompat.EMAIL_ADDRESS.matcher(this).matches()
+}
 
 class ExampleUnitTest {
     @Test
@@ -10,5 +25,37 @@ class ExampleUnitTest {
         Assert.assertEquals(4, 2 + 2)
     }
 
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue("name@email.com".isValidEmail())
+    }
 
+}
+
+class MockedContextTest {
+
+    val KEY_DARK_THEME = "dark_theme"
+    val KEY_DARK_DEFAULT = 0
+
+    @Mock val mockContext: Context = mock()
+
+    @Mock val mockPrefs: SharedPreferences = mock()
+
+    @Mock val mockEditor: SharedPreferences.Editor = mock()
+
+    @Before
+    @Throws(Exception::class)
+    fun before() {
+        Mockito.`when`(mockContext.getSharedPreferences(anyString(), anyInt()))
+            .thenReturn(mockPrefs)
+        Mockito.`when`(mockContext.getSharedPreferences(anyString(), anyInt()).edit())
+            .thenReturn(mockEditor)
+        Mockito.`when`(mockPrefs.getInt(KEY_DARK_THEME, KEY_DARK_DEFAULT))
+            .thenReturn(KEY_DARK_DEFAULT)
+    }
+
+    @Test
+    fun `validate default theme status`() {
+        assertEquals(mockPrefs.getInt(KEY_DARK_THEME, KEY_DARK_DEFAULT), KEY_DARK_DEFAULT)
+    }
 }
